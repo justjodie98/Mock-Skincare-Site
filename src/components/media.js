ec1d093da8c2e535aa51bac3946fbd14d283ce20
Jodie Koffi <jodie.koffi@thebeansgroup.com>
2023-08-23 16:33:27 +0100

    Add media component

---
+++ src/components/media.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Media() {
+  return (
+    <article className="gallery">
+      <h1>Follow us on Instagram</h1>
+      <section className="media">
+        <img src="https://images.pexels.com/photos/5938217/pexels-photo-5938217.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        <img src="https://images.pexels.com/photos/6781117/pexels-photo-6781117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        <img src="https://images.pexels.com/photos/5938590/pexels-photo-5938590.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        <img src="https://images.pexels.com/photos/7480007/pexels-photo-7480007.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+        <img src="https://images.pexels.com/photos/7479819/pexels-photo-7479819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+        <img src="https://images.pexels.com/photos/8140902/pexels-photo-8140902.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+        <img src="https://images.pexels.com/photos/8128062/pexels-photo-8128062.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+        <img src="https://images.pexels.com/photos/8142187/pexels-photo-8142187.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+      </section>
+    </article>
+  );
+}
